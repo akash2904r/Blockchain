512605e8becbb4abee261607601ede5ecf617069
Akash <akash2904r@gmail.com>
2024-09-12 22:01:12 +0530

    Updated require condition

--- Solidity/FundMe.sol
+++ Solidity/FundMe.sol
@@ -40,7 +40,7 @@ contract FundMe {
     uint256 public minimumUSD = 50;
 
     function fund() public payable {
-        require(msg.value > minimumUSD, "Didn't send enough !!!");
+        require(msg.value >= minimumUSD, "Didn't send enough !!!");
     }
 
     function withdraw() {}
