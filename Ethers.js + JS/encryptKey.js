77d2daaf37de4ef7ca1d2f500930b18379c9cbdc
Akash <akash2904r@gmail.com>
2024-11-05 21:34:45 +0530

    Add comment in encryptKey.js

--- Ethers.js + JS/encryptKey.js
+++ Ethers.js + JS/encryptKey.js
@@ -3,10 +3,11 @@ const ethers = require("ethers");
 
 require("dotenv").config();
 
+// Delete the PRIVATE_KEY and PRIVATE_KEY_PASSWORD from the .env file after running this once
 async function main() {
     const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
     const encryptedJsonKey = await wallet.encrypt(process.env.PRIVATE_KEY_PASSWORD);
-    
+
     fs.writeFileSync("./.encryptedKey.json", encryptedJsonKey);
 }
 
