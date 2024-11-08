8b3a5f2eff2d673de442c4c8531822b3abd86516
Akash <akash2904r@gmail.com>
2024-11-08 19:08:41 +0530

    Add code to interact with deployed contract

--- Hardhat (Simple Storage)/ignition/modules/SimpleStorage.js
+++ Hardhat (Simple Storage)/ignition/modules/SimpleStorage.js
@@ -3,5 +3,9 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 module.exports = buildModule("SimpleStorageModule", (m) => {
     const simpleStorageContract = m.contract("SimpleStorage");
 
+    m.call(simpleStorageContract, "retrieve");
+
+    m.call(simpleStorageContract, "store", [5]);
+
     return { simpleStorageContract };
 })
