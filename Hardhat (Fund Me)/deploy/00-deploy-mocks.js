de3e289a80ddf07597d61bd9a6f0400129367013
Akash <akash2904r@gmail.com>
2024-11-10 20:41:03 +0530

    Add 00-deploy-mocks.js file

---
+++ Hardhat (Fund Me)/deploy/00-deploy-mocks.js
@@ -0,0 +1,29 @@
+const { network } = require("hardhat");
+const { 
+    developmentChains, 
+    DECIMALS, 
+    INITIAL_ANSWER 
+} = require("../helper-hardhat-config");
+
+// Mocking --> If a contract doesn't exist, we deploy a minimal version of it for our local testing
+// Since our local network (i.e., hardhat or localhost) does have a AggregatorV3Interface deployed, we manually deploy a mock
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments;
+    const { deployer } = await getNamedAccounts();
+
+    if (developmentChains.includes(network.name)) {
+        log("Local network detected! Deploying mocks...");
+
+        await deploy("MockV3Aggregator", {
+            contract: "MockV3Aggregator", // Not mandatory
+            from: deployer,
+            log: true,
+            args: [DECIMALS, INITIAL_ANSWER],
+        });
+
+        log("Mocks deployed!");
+        log("--------------------------------------------------");
+    }
+}
+
+module.exports.tags = ["all", "mocks"]
