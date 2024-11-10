fe01fd6b5aa6826352b240152bf7dd1f0254c25b
Akash <akash2904r@gmail.com>
2024-11-10 20:42:32 +0530

    Update hardhat.config.js file

--- Hardhat (Fund Me)/hardhat.config.js
+++ Hardhat (Fund Me)/hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-ignition");
 require("hardhat-gas-reporter");
 require("solidity-coverage");
 require("hardhat-deploy");
@@ -6,6 +7,46 @@ require("hardhat-deploy");
 require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
+
 module.exports = {
-  solidity: "0.8.27",
+  defaultNetwork: "hardhat",
+  solidity: {
+    compilers: [{ version: "0.8.8" }, { version: "0.6.6" }]
+  },
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+      blockConfirmations: 6,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      chainId: 31337,
+    }
+  },
+  gasReporter: {
+    enabled: false,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // This will by default take the first account as deployer
+      11155111: 1, // This means, on the sepolia network the second account is taken as deployer
+    },
+    user: {
+      default: 1, // This on the other hand would take the second account as user by default
+    },
+  }
 };
