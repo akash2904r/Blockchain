b56bb904fc0489af248925a8ea45033f884f2d7f
Akash <akash2904r@gmail.com>
2024-11-15 19:45:09 +0530

    Add Entrance Fee and Players variables alongside getters

--- Hardhat (Lottery)/contracts/Raffle.sol
+++ Hardhat (Lottery)/contracts/Raffle.sol
@@ -2,5 +2,18 @@
 pragma solidity ^0.8.7;
 
 contract Raffle {
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
 
+    constructor (uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function getEntranceFee() public view returns(uint256) {
+        return i_entranceFee;
+    }
+
+    function getPlayer(uint256 index) public view returns(address) {
+        return s_players[index];
+    }
 }
