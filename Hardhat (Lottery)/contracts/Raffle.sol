f3a33d36eabf75499e720d6a7c125eef87007906
Akash <akash2904r@gmail.com>
2024-11-15 19:56:17 +0530

    Add Enter Raffle method to contract

--- Hardhat (Lottery)/contracts/Raffle.sol
+++ Hardhat (Lottery)/contracts/Raffle.sol
@@ -1,12 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+error Raffle__NotEnoughETH();
+
 contract Raffle {
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
 
+    // Good Practice: Name events with the function name reversed
+    event RaffleEnter(address indexed player);
+
     constructor (uint256 entranceFee) {
         i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughETH();
+        }
+        s_players.push(payable(msg.sender));
+        // Emit an event when we update the dynamic array i.e., players
+        emit RaffleEnter(msg.sender);
     }
 
     function getEntranceFee() public view returns(uint256) {
