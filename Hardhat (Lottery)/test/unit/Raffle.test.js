4fab061ddcf5b253b61db3a7412d7a1ae6212d01
Akash <akash2904r@gmail.com>
2024-11-18 19:32:56 +0530

    Add test for raffle constructor and enter raffle method

---
+++ Hardhat (Lottery)/test/unit/Raffle.test.js
@@ -0,0 +1,33 @@
+const { assert, expect } = require("chai");
+const { network, ethers, getNamedAccounts, deployments } = require("hardhat");
+const { developmentChains, networkConfig } = require("../../helper-hardhat-config");
+
+!developmentChains.includes(network.name)
+    ? describe.skip
+    : describe("Raffle Unit Tests", function () {
+        let raffle, vrfCoordinatorV2Mock;
+        const chainId = network.config.chainId;
+
+        beforeEach(async function () {
+            const { deployer } = await getNamedAccounts();
+            await deployments.fixture(["all"]);
+            raffle = await ethers.getContract("Raffle", deployer);
+            vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer);
+        })
+
+        describe("constructor", function () {
+            it("Initializes the raffle correctly", async function () {
+                // Ideally we make our tests have just 1 assert per it
+                const raffleState = await raffle.getRaffleState();
+                const interval = await raffle.getInterval();
+                assert.equal(raffleState.toString(), "0");
+                assert.equal(interval.toString(), networkConfig[chainId]["interval"]);
+            })
+        })
+
+        describe("enterRaffle", function () {
+            it("Reverts if you don't pay enough", async function () {
+                await expect(raffle.enterRaffle()).to.be.revertedWith("Raffle__NotEnoughETH");
+            })
+        })
+    })
