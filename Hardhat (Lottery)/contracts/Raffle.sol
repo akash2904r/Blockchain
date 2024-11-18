bcad2e4823e031e00e52a0d1c2a4d72be09a7c1d
Akash <akash2904r@gmail.com>
2024-11-18 19:30:57 +0530

    Add getter for interval variable

--- Hardhat (Lottery)/contracts/Raffle.sol
+++ Hardhat (Lottery)/contracts/Raffle.sol
@@ -180,6 +180,10 @@ contract Raffle is VRFConsumerBaseV2, AutomationCompatibleInterface {
         return s_lastTimeStamp;
     }
 
+    function getInterval() public view returns(uint256) {
+        return i_interval;
+    }
+
     // Pure Functions
     function getNumWords() public pure returns(uint256) {
         return NUM_WORDS;
