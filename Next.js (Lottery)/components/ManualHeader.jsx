be89aebb266d4cc244f4274777a854d074619ce3
Akash <akash2904r@gmail.com>
2024-11-22 21:56:58 +0530

    Add connect button in ManualHeader

--- Next.js (Lottery)/components/ManualHeader.jsx
+++ Next.js (Lottery)/components/ManualHeader.jsx
@@ -1,11 +1,15 @@
 import { useMoralis } from "react-moralis";
 
 export default function ManualHeader() {
-    const { enableWeb3 } = useMoralis();
+    const { enableWeb3, account } = useMoralis();
 
     return (
         <div>
-            ManualHeader
+            {account ? (
+                <div>Connected to: {account.slice(0, 6)}...{account.slice(-4)}</div>
+            ) : (
+                <button onClick={async () => await enableWeb3()}>Connect</button>
+            )}
         </div>
     );
 }
