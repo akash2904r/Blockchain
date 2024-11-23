a0e71c370b00a097e473c4687caa672dbc0deff6
Akash <akash2904r@gmail.com>
2024-11-23 19:16:25 +0530

    Use Header instead of ManualHeader in Home page

--- Next.js (Lottery)/app/page.js
+++ Next.js (Lottery)/app/page.js
@@ -1,13 +1,15 @@
 "use client";
 
 import { MoralisProvider } from "react-moralis";
-import ManualHeader from "@/components/ManualHeader";
+// import ManualHeader from "@/components/ManualHeader";
+import Header from "@/components/Header";
 
 export default function Home() {
     return (
         <div>
             <MoralisProvider initializeOnMount={false}>
-                <ManualHeader />
+                {/* <ManualHeader /> */}
+                <Header />
             </MoralisProvider>
         </div>
     );
