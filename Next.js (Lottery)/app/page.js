2fca254d629d165250f995c88232680f8e3e8c16
Akash <akash2904r@gmail.com>
2024-11-24 21:22:55 +0530

    Add LotteryEntrance component to Home page

--- Next.js (Lottery)/app/page.js
+++ Next.js (Lottery)/app/page.js
@@ -1,15 +1,21 @@
 "use client";
 
 import { MoralisProvider } from "react-moralis";
+import { NotificationProvider } from "web3uikit";
+
 // import ManualHeader from "@/components/ManualHeader";
 import Header from "@/components/Header";
+import LotteryEntrance from "@/components/LotteryEntrance";
 
 export default function Home() {
     return (
         <div>
             <MoralisProvider initializeOnMount={false}>
-                {/* <ManualHeader /> */}
-                <Header />
+                <NotificationProvider>
+                    {/* <ManualHeader /> */}
+                    <Header />
+                    <LotteryEntrance />
+                </NotificationProvider>
             </MoralisProvider>
         </div>
     );
