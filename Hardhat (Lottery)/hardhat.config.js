9e8d617f8189d9608d3236c9daf8193fb1b889df
Akash <akash2904r@gmail.com>
2024-11-25 21:58:59 +0530

    Add localhost in networks field

--- Hardhat (Lottery)/hardhat.config.js
+++ Hardhat (Lottery)/hardhat.config.js
@@ -21,6 +21,9 @@ module.exports = {
       chainId: 31337,
       blockConfirmations: 1,
     },
+    localhost: {
+      chainId: 31337,
+    },
     sepolia: {
       chainId: 11155111,
       blockConfirmations: 6,
