d1ab0ba939b78d143ff2d9c6e4091064d42df40c
Akash <akash2904r@gmail.com>
2024-11-26 21:17:38 +0530

    Add styling to Header component

--- Next.js (Lottery)/components/Header.jsx
+++ Next.js (Lottery)/components/Header.jsx
@@ -2,9 +2,11 @@ import { ConnectButton } from "web3uikit";
 
 export default function Header() {
     return (
-        <div>
-            <h1>Raffle Lottery</h1>
-            <ConnectButton moralisAuth={false} />
+        <div className="p-3 border-b-2 border-[#363636] flex items-center">
+            <h1 className="p-4 font-bold text-3xl text-[#6689bf]">Raffle Lottery</h1>
+            <div className="ml-auto py-2 px-4">
+                <ConnectButton moralisAuth={false} className="bg-black" />
+            </div>
         </div>
     );
 }
