c92f9cc270efa99bf73f5b05e84fbc134c81e982
Akash <akash2904r@gmail.com>
2024-12-05 20:56:47 +0530

    Configure hardhat with new parameters

--- Hardhat DeFi/hardhat.config.js
+++ Hardhat DeFi/hardhat.config.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL || "";
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "";
@@ -17,15 +17,23 @@ const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "";
 module.exports = {
   defaultNetwork: "hardhat",
   solidity: {
-    compilers: [{ version: "0.8.8" }, { version: "0.6.6" }]
+    compilers: [
+      { version: "0.8.0" }, { version: "0.6.12" }, { version: "0.4.19" }
+    ]
   },
   networks: {
-    sepolia: {
-      url: SEPOLIA_RPC_URL,
-      accounts: [PRIVATE_KEY],
-      chainId: 11155111,
-      blockConfirmations: 6,
+    hardhat: {
+      chainId: 31337,
+      forking: {
+        url: MAINNET_RPC_URL,
+      },
     },
+    // sepolia: {
+    //   url: SEPOLIA_RPC_URL,
+    //   accounts: [PRIVATE_KEY],
+    //   chainId: 11155111,
+    //   blockConfirmations: 6,
+    // },
     localhost: {
       url: "http://127.0.0.1:8545/",
       chainId: 31337,
