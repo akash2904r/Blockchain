22a605bc61a3ddc236ed204a48eb61e1ddd10c02
Akash <akash2904r@gmail.com>
2024-12-06 19:33:12 +0530

    Complete getPool function

--- Hardhat DeFi/scripts/aaveBorrow.js
+++ Hardhat DeFi/scripts/aaveBorrow.js
@@ -3,9 +3,18 @@ const { getWeth } = require("./getWeth");
 
 async function main() {
     await getWeth();
+    const { deployer } = await getNamedAccounts();
+    // Pool Provider Address: 0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e
+    const pool = await getPool(deployer);
+    console.log(`Pool Address: ${pool.address}`)
 }
 
-async function getPool() {}
+async function getPool(account) {
+    const poolAddressesProvider = await ethers.getContractAt("IPoolAddressesProvider", "0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e", account);
+    const poolAddress = await poolAddressesProvider.getPool();
+    const pool = await ethers.getContractAt("IPool", poolAddress, account);
+    return pool;
+}
 
 main()
     .then(() => process.exit(0))
